6eb9298d75a610d09089686763339ad00aee8469
brnooliveira <brno.oliveiras@gmail.com>
2022-06-05 21:59:54 -0300

    fist commit

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+    belongs_to :author
+    has_many :comments
+
+    validates :title, presence: true, uniqueness: true
+    validates :body, presence: true
+
+    
+end
